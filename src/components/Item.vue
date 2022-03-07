8fed4ee5fb509390da86e559fe5642064cd4b70a
wangjiahui <wangjiahui@tom.com>
2022-03-07 15:12:10 +0800

    TodoList Style Complete

---
+++ src/components/Item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <li>
+      <div class="view">
+        <input type="checkbox" class="toggle" />
+        <label>content</label>
+        <button class="destory"></button>
+      </div>
+      <input type="text" class="edit" />
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Item",
+  data() {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
